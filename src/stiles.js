886f6791bdabc677bf446b513116f379e116709a
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2021-04-28 11:25:20 +0300

    redy-hw-8

---
+++ src/stiles.js
@@ -0,0 +1,72 @@
+import settings from './gallery-items.js';
+
+const galleryRef = document.querySelector('.js-gallery');
+const lightboxRef = document.querySelector('.js-lightbox');
+const lightboxImageRef = document.querySelector('.js-lightbox .lightbox__image');
+const coseModalBtnRef = document.querySelector('.js-lightbox .lightbox__button');
+const lightboxOverlayRef = document.querySelector('.js-lightbox .lightbox__overlay');
+const imageMarkupRef = createImageMarkup(settings);
+
+galleryRef.insertAdjacentHTML('beforeend', imageMarkupRef);
+galleryRef.addEventListener('click', onClickOpenLightbox);
+coseModalBtnRef.addEventListener('click', onClickCloseModal);
+lightboxOverlayRef.addEventListener('click', onClickCloseModal);
+window.addEventListener('keydown', );
+
+function createImageMarkup(settings) {
+    return settings.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${original}"
+        >
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    }).join('')
+};
+
+function onClickOpenLightbox(e) {
+    e.preventDefault();
+    if (e.currentTarget === e.target) {
+        return;
+    };
+
+    window.addEventListener('keydown', onEscKeyPress);
+    lightboxRef.classList.add('is-open');
+    console.log(getImageUrlAndAlt(e));
+
+};
+
+function getImageUrlAndAlt(e) {
+    const bigImgUrl = e.target.dataset.source;
+    const bigImgAlt = e.target.alt;
+
+    lightboxImageRef.src = bigImgUrl;
+    lightboxImageRef.alt = bigImgAlt;
+};
+
+function onClickCloseModal(e) {
+    if (e.target !== e.currentTarget) {
+        return;
+    };
+    window.removeEventListener('keydown', onEscKeyPress);
+    closeModal()
+}
+
+function onEscKeyPress(e) {
+    if (e.code !== 'Escape') {
+        return;
+    }
+    closeModal();
+}
+
+function closeModal() {
+    lightboxImageRef.src = "";
+    lightboxRef.classList.remove('is-open');
+}
